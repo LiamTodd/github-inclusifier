48694fbd1ddcf0971d4e84a000e11f7fc1bbe4ff
LiamTodd <toddliam26@gmail.com>
2023-07-31 00:21:18 +1000

    Resolve double repo root issue.

--- backend/backend/utils/reconstruct_file_structure_utils.py
+++ backend/backend/utils/reconstruct_file_structure_utils.py
@@ -1,7 +1,11 @@
 def reconstruct_file_structure(flat_file_data, repo_root):
     flat_file_data_list = list(flat_file_data.values())
     root = flat_file_data.get(repo_root)
-    return [reconstruct_file_structure_aux(flat_file_data_list, root)]
+    return [
+        resolve_double_root_structure(
+            reconstruct_file_structure_aux(flat_file_data_list, root)
+        )
+    ]
 
 
 def reconstruct_file_structure_aux(flat_file_data_list, parent):
@@ -15,3 +19,13 @@ def reconstruct_file_structure_aux(flat_file_data_list, parent):
             reconstruct_file_structure_aux(flat_file_data_list, child)
     parent["children"] = children
     return parent
+
+
+def resolve_double_root_structure(reconstructed_file_structure):
+    # This function changes the file structure from:
+    # root___root-2___repo-content
+    # to:
+    # root___repo-content
+    duplicated_root = reconstructed_file_structure["children"][0]
+    reconstructed_file_structure["children"] = duplicated_root["children"]
+    return reconstructed_file_structure
