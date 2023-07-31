0ae4e5373b411cc2149e113515e838b6b4e9c948
LiamTodd <toddliam26@gmail.com>
2023-07-31 14:52:46 +1000

    Implement wbpm backend.

--- backend/backend/utils/language_analysis_utils.py
+++ backend/backend/utils/language_analysis_utils.py
@@ -27,4 +27,23 @@ def sub_string_pattern_match(file_data):
 
 
 def word_boundary_pattern_match(file_data):
+    for file in file_data.values():
+        if (
+            file.get("content") is not None
+            and file.get("content") != FAILED_FILE_READ_WARNING
+        ):
+            cleaned_file_content = file.get("content").lower()
+            file["wbpm_matches"] = {}
+            for category_name, category_list in NON_INCLUSIVE_LANGUAGE_TERMS.items():
+                file["wbpm_matches"][category_name] = {}
+                for term in category_list:
+                    # find start index of all word-boundary matches
+                    matching_start_indexes = [
+                        m.start()
+                        for m in re.finditer(rf"\b{term}\b", cleaned_file_content)
+                    ]
+                    if len(matching_start_indexes) > 0:
+                        file["wbpm_matches"][category_name][
+                            term
+                        ] = matching_start_indexes
     return file_data
